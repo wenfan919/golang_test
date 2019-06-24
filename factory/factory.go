0373e05e720fd41177b4985cb3f1c7eeed048a0d
yanyong <yanyongc@yonyou.com>
2019-06-24 09:57:42 +0800

    del the main function

--- factory/factory.go
+++ factory/factory.go
@@ -1,7 +1,5 @@
 package factory
 
-import "fmt"
-
 type File struct {
 	fd      int     // 文件描述符
 	name    string  // 文件名
@@ -17,9 +15,9 @@ func NewFile(fd int, name string) *File {
 	return &File{fd-1, name}
 }
 
-func main(){
-
-	f := NewFile(10, "./test.txt")
-
-	fmt.Println("f: ", f)
-}
+//func main(){
+//
+//	f := NewFile(10, "./test.txt")
+//
+//	fmt.Println("f: ", f)
+//}
